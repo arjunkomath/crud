368a02483ce0fa7455cbdebf1f46005a0cff2d87
Arjun <arjun@Arjuns-MacBook-Pro.local>
2015-08-10 01:45:52 +0530

    Added Basic CRUD

---
+++ lib/CRUD.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace CRUD;
+
+class CRUD {
+
+    public function read($table, $id = null) {
+        $data = \ORM::for_table($table);
+        if($id){
+            $data->where_id_is($id);
+        }
+        return $data->find_array();
+    }
+
+    public function save($table, $params, $id=null) {
+        $table_object = \ORM::for_table($table);
+        if($id) {
+            $table_object->where_id_is($id);
+        }
+        $table_row = $table_object->create();
+        foreach ($params as $key => $value) {
+            $table_row->$key = $value;
+        }
+        $table_row->save();
+        return $table_row->id();
+    }
+
+    public function find($table, $key, $value) {
+        return \ORM::for_table($table)
+            ->where($key, $value)
+            ->find_array();
+    }
+
+    public function delete($table, $id) {
+        \ORM::for_table($table)->where_id_is($id)->delete();
+        return true;
+    }
+
+}
+
+?>
